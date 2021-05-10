4e68e9c1bcb52dde4c8ed471fb965ced44174296
Daniel <daniel.palacz@gmail.com>
2021-05-10 15:57:56 +0200

    earch engine for finding Polish monuments (0.3).

--- poznajmy_polskie_zabytki/views.py
+++ poznajmy_polskie_zabytki/views.py
@@ -20,6 +20,26 @@ from poznajmy_polskie_zabytki.db import get_db
 bp = Blueprint("views", __name__)
 
 
+def create_sql_query(*, city="", parish="", county="", keyword="") -> str:
+    sql_query = "SELECT * from zabytki where"
+    if city:
+        sql_query += " miejscowosc='" + city + "'"
+    if parish:
+        if city:
+            sql_query += " and"
+        sql_query += " gmina like '%" + parish + "%'"
+    if county:
+        if city or parish:
+            sql_query += " and"
+        sql_query += " powiat='" + county + "'"
+    if keyword:
+        if city or parish or county:
+            sql_query += " and"
+        sql_query += " ( nazwa like '%" + keyword + "%'" + " or funkcja like '%" + keyword + "%'"
+        sql_query += " or wojewodztwo like '%" + keyword + "%'" + " or chronologia like '%" + keyword + "%' )"
+    return sql_query
+
+
 @bp.route('/')
 def index():
     return render_template('index.html')
@@ -27,13 +47,15 @@ def index():
 
 @bp.route('/wyszukaj/')
 def wyszukaj():
-    if tuple(request.args):
-        city = request.args.get("miasto")
+    city = request.args.get("miasto", "")
+    parish = request.args.get("gmina", "")
+    county = request.args.get("powiat", "")
+    keyword = request.args.get("dowolneslowo", "")
+    if city or parish or county or keyword:
         try:
             db = get_db()
-            c = db.cursor()
-            sql_query = f"SELECT * from zabytki where miejscowosc='{city}'"
-            output = c.execute(sql_query).fetchall()
+            sql_query = create_sql_query(city=city, parish=parish, county=county, keyword=keyword)
+            output = db.execute(sql_query).fetchall()
         except sqlite3.Error:
             abort(500)
         items = [tuple([id_temp+1]) + tuple(item) for id_temp, item in enumerate(output)]
